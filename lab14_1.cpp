d6ababcdecd99ddf56142d4d36bdc605dae7ee70
nithikon4410 <nithikon_jansanitsri@cmu.ac.th>
2021-02-02 15:36:09 +0700

    lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,9 +1,28 @@
-include <iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
+	int a = 5;
+	string b = "A";
+	string &c = b;
+	int *x = &a;
+	string *y = &b;
+	int *z = x;
+	
+	cout << a << " " << b << " " << c << " " << &x << " "<< &y << " "<< &z << endl;
+	cout << &a << " " << &b << " " << &c << " " << &x << " "<< &y << " "<< &z << endl;
+	
+	c = "F";
+	cout << a << " " << b << " " << c << " " << &x << " "<< &y << " "<< &z << endl;
 
-	
-	
+	b = "W";
+	cout << a << " " << b << " " << c << " " << &x << " "<< &y << " "<< &z << endl;
+
+	a = 6;
+	cout << a << " " << b << " " << c << " " << &x << " "<< &y << " "<< &z << endl;
+
+	a = 7;
+	cout << a << " " << b << " " << c << " " << &x << " "<< &y << " "<< &z << endl;
 	return 0;
 }
